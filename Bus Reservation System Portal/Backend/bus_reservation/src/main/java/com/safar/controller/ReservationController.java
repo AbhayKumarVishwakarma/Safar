0bc0a25d32571869aaa1a8b51ca140f5aaf15578
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-05-04 01:51:32 +0530

    day-2

---
+++ Bus Reservation System Portal/Backend/bus_reservation/src/main/java/com/safar/controller/ReservationController.java
@@ -0,0 +1,37 @@
+package com.safar.controller;
+
+
+import com.safar.exception.ReservationException;
+import com.safar.model.Reservation;
+import com.safar.service.ReservationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/reservation")
+public class ReservationController {
+
+    @Autowired
+    private ReservationService service;
+
+    @PostMapping("/add")
+    public ResponseEntity<Reservation> addReservation(@Valid @RequestBody Reservation reservation){
+        Reservation reservation1 = service.addReservation(reservation);
+
+        return new ResponseEntity<>(reservation1, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Reservation> updateReservation(@Valid @RequestBody Reservation reservation, @PathVariable Integer rid) throws ReservationException {
+        Reservation reservation1 = service.updateReservation(reservation, rid);
+
+        return  new ResponseEntity<>(reservation1, HttpStatus.ACCEPTED);
+    }
+
+
+
+}
