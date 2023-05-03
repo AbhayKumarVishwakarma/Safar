63d2c87ae1f6de779263da077f69049df00a3b2a
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-05-03 16:07:55 +0530

    update

--- Bus/src/main/java/com/bus/model/Reservation.java
+++ Bus/src/main/java/com/bus/model/Reservation.java
@@ -1,4 +1,13 @@
 package com.bus.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Reservation {
 }
