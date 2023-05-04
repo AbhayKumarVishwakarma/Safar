cf495c58b82990b3df3f6f0604a6c8458d1ac27d
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-05-05 02:39:49 +0530

    day-3 Resevation Updates

--- Bus Reservation System Portal/Backend/bus_reservation/src/main/java/com/safar/exception/GlobalExceptionHandler.java
+++ Bus Reservation System Portal/Backend/bus_reservation/src/main/java/com/safar/exception/GlobalExceptionHandler.java
@@ -43,4 +43,15 @@ public class GlobalExceptionHandler {
         details.setDetails(w.getDescription(false));
         return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler
+    public ResponseEntity<MyErrorDetails> reservationExceptionHandler(ReservationException ex, WebRequest w){
+        MyErrorDetails details = new MyErrorDetails();
+
+        details.setDetails(ex.getMessage());
+        details.setTime(LocalDateTime.now());
+        details.setDetails(w.getDescription(false));
+
+        return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
+    }
 }
--- Bus Reservation System Portal/Backend/bus_reservation/src/main/java/com/safar/repository/BusRepository.java
+++ Bus Reservation System Portal/Backend/bus_reservation/src/main/java/com/safar/repository/BusRepository.java
@@ -1,4 +1,7 @@
 package com.safar.repository;
 
-public class BusRepository {
+import com.safar.model.Bus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BusRepository extends JpaRepository<Bus, Integer> {
 }
