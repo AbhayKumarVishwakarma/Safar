152db8315985a8458d8d04bb45e8da4fc2fde507
Yash Nimkar <yashnimkar@Yashs-MacBook-Pro.local>
2023-05-09 01:00:11 +0530

    done

--- Bus Reservation System Portal/Frontend/src/scripts/UserSignup.js
+++ Bus Reservation System Portal/Frontend/src/scripts/UserSignup.js
@@ -1,40 +1,46 @@
 
 
     document.querySelector("form").addEventListener("submit", myfun)
-    let arr = JSON.parse(localStorage.getItem("PDetails")) || [];
+    // let arr = JSON.parse(localStorage.getItem("PDetails")) || [];
 
     function myfun(event) {
         event.preventDefault()
 
-        let pName = document.querySelector("#name").value;
+        let fName = document.querySelector("#firstname").value;
+        let lName = document.querySelector("#lastname").value;
         let pEmail = document.querySelector("#email").value;
+        
         let pReEmail = document.querySelector("#reEmail").value;
+        let mobile = document.querySelector("#mobile").value;
         let pPassword = document.querySelector("#password").value;
         // let pBdate = document.querySelector("#date").value;
         // let pCountry = document.querySelector("#country").value;
         // let pState = document.querySelector("#state").value;
 
-        if (pName == "" || pEmail == "" ) {
+        // if (pName == "" || pEmail == "" ) {
 
-            alert("Please fill correct details")
-        }
+        //     alert("Please fill correct details")
+        // }
         // if(pEmail != pReEmail) {
         //     alert("Email not match")
         // }
-        else {
+         {
             let obj = {
-                pName,
-                pEmail,
-                pPassword,
+                "firstName":fName,
+                "lastName":lName,
+                "mobile":mobile,
+                "email":pEmail,
+                "password":pPassword
                 // pBdate,
                 // pCountry,
                 // pState
             }
-            arr.push(obj)
-            console.log(arr)
-            localStorage.setItem("pDetails", JSON.stringify(arr))
-            alert("Sign Up successful")
-            location.href = "index.html"
+            // arr.push(obj)
+            // console.log(arr)
+            // localStorage.setItem("pDetails", JSON.stringify(arr))
+            // alert("Sign Up successful")
+            // location.href = "index.html"
+            addUser(obj)
         }
 
     }
@@ -60,3 +66,25 @@
 
     // })
 
+    async function addUser(obj){
+        try {      
+            const res = await fetch("http://localhost:8999/safar/user/register",{
+                method: 'POST',
+                body: JSON.stringify(obj),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+    
+            const out = await res.json();
+            //bus added , 2sec delay
+            alert("User added successfully!")
+            setTimeout(function() {
+                window.location.href = "index.html";
+              }, 2000); 
+    
+        } catch (error) {
+            //showing alert for error
+            alert(error);
+        }
+    }
